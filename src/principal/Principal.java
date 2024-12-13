264e6f94e05c45184d0b523453f5d392454d4ba5
KoraEXE <korawitrolarieta@gmail.com>
2024-12-13 12:04:12 +0100

    Paso 2

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("me cago en la puta");
+		System.out.println("i love bego");
 	}
 
 }
